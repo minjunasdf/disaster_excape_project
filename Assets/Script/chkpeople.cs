c9a1b86b6dde4b17528d93e737f9b923709102f5
austinlee04 <56777269+austinlee04@users.noreply.github.com>
2023-04-03 20:53:02 +0900

    Detect People
    구윤환 작품

---
+++ Assets/Script/chkpeople.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chkpeople : MonoBehaviour
+{
+    public int peoplenum;
+    Vector3[] roompos = new Vector3[9];
+    float[] pos = new float[3];
+    float roomsize = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos[0]=7f;
+        pos[1]=0f;
+        pos[2]=-7f;
+        for(int i = 0; i < 9; i++)
+        {
+            roompos[i] = new Vector3(pos[i / 3], 1, pos[(10-i) % 3]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "RescueNeeded")
+        {
+            peoplenum++;
+            //Debug.Log("123"+peoplenum);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "RescueNeeded") peoplenum--;
+    }
+}
