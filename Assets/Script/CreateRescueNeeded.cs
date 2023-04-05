5bdce172262e3ac54b90b432c644440e8504f766
austinlee04 <56777269+austinlee04@users.noreply.github.com>
2023-04-05 18:42:57 +0900

    oliu sujeng
    adfasdfasfdasdf

--- Assets/Script/CreateRescueNeeded.cs
+++ Assets/Script/CreateRescueNeeded.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Random = UnityEngine.Random;
-
 public class CreateRescueNeeded : MonoBehaviour
 {
     public GameObject RescueObject;
-    public GameObject[] person;
+    public GameObject[] Rooms = new GameObject[9]; //방 받아오는 변수
+    public GameObject[] person = new GameObject[5];
 
     void Start()
     {
         int random_number;
-        Vector3[] coord = new[] { new Vector3(7, 1, 7), new Vector3(7, 1, -7), new Vector3(0, 1, 0), new Vector3(0, 1, 7), new Vector3(0, 1, -7), new Vector3(-7, 1, 0), new Vector3(-7, 1, 7), new Vector3(-7, 1, -7) };
+        int i;
+        Vector3[] coord = new Vector3[] {new Vector3(7,1,0), //방 위치 반영되도록 변경!
+                                         new Vector3(7, 1, 7),
+                                         new Vector3(7, 1, -7),
+                                        new Vector3(0, 1, 0),
+                                        new Vector3(0, 1, 7), 
+                                        new Vector3(0, 1, -7), 
+                                        new Vector3(-7, 1, 0), 
+                                        new Vector3(-7, 1, 7), 
+                                        new Vector3(-7, 1, -7) };
 
-        for (int i=0;i<5;i++)
+        for (i=0;i<5;i++)
         {
-            random_number = Random.Range(0,8);
+            random_number = Random.Range(0,9);
             Vector3 random_coord = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
-            person[i]=Instantiate(RescueObject, coord[random_number]+random_coord, Quaternion.identity);
+            person[i] = Instantiate(RescueObject, coord[random_number] + random_coord, Quaternion.identity);
         }
 
     }
--- Assets/Script/MoveToSomewhere.cs
+++ Assets/Script/MoveToSomewhere.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveToSomewhere : MonoBehaviour
 {
+    Vector3[] coord = new[] { new Vector3(7, 1, 0), new Vector3(7, 1, 7), new Vector3(7, 1, -7), new Vector3(0, 1, 0), new Vector3(0, 1, 7), new Vector3(0, 1, -7), new Vector3(-7, 1, 0), new Vector3(-7, 1, 7) };
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,9 @@ public class MoveToSomewhere : MonoBehaviour
     void Update()
     {
         
+    }
+    public void GetMoveCommand(int n)// 방 번호를 입력받아 그 위치로 이동
+    {
+
     }
 }
