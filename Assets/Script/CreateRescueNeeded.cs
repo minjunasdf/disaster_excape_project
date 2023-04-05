f5128a5915972ffcb4e9344540efd05f0e075ffe
austinlee04 <56777269+austinlee04@users.noreply.github.com>
2023-04-05 18:52:39 +0900

    room byunsu
    adfasd

--- Assets/Script/CreateRescueNeeded.cs
+++ Assets/Script/CreateRescueNeeded.cs
@@ -13,21 +13,11 @@ public class CreateRescueNeeded : MonoBehaviour
     {
         int random_number;
         int i;
-        Vector3[] coord = new Vector3[] {new Vector3(7,1,0), //방 위치 반영되도록 변경!
-                                         new Vector3(7, 1, 7),
-                                         new Vector3(7, 1, -7),
-                                        new Vector3(0, 1, 0),
-                                        new Vector3(0, 1, 7), 
-                                        new Vector3(0, 1, -7), 
-                                        new Vector3(-7, 1, 0), 
-                                        new Vector3(-7, 1, 7), 
-                                        new Vector3(-7, 1, -7) };
-
         for (i=0;i<5;i++)
         {
             random_number = Random.Range(0,9);
             Vector3 random_coord = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
-            person[i] = Instantiate(RescueObject, coord[random_number] + random_coord, Quaternion.identity);
+            person[i] = Instantiate(RescueObject, Rooms[random_number].transform.position + random_coord, Quaternion.identity);
         }
 
     }
