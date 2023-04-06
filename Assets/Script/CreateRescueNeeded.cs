0c92e9a7a51c657de88bf3494ae022a359c3f2ef
minjunasdf <62454927+minjunasdf@users.noreply.github.com>
2023-04-06 20:26:36 +0900

    make MoveToSomewhere.cs & ExcapeAgent.cs

--- Assets/Script/CreateRescueNeeded.cs
+++ Assets/Script/CreateRescueNeeded.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using Random = UnityEngine.Random;
+
+
 public class CreateRescueNeeded : MonoBehaviour
 {
     public GameObject RescueObject;
-    public GameObject Exit;
+    public GameObject[] Exit = new GameObject[4];
     public GameObject[] Rooms = new GameObject[9]; //방 받아오는 변수
     public GameObject[] person = new GameObject[5];
 
@@ -19,7 +20,6 @@ public class CreateRescueNeeded : MonoBehaviour
             random_number = Random.Range(0,9);
             Vector3 random_coord = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
             person[i] = Instantiate(RescueObject, Rooms[random_number].transform.position + random_coord, Quaternion.identity);
-            person[i].GetComponent<MoveToSomewhere>().target = Exit;
         }
 
     }
--- Assets/Script/MoveToSomewhere.cs
+++ Assets/Script/MoveToSomewhere.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class MoveToSomewhere : MonoBehaviour
 {
     UnityEngine.AI.NavMeshAgent agent;
-    Vector3[] coord = new[] { new Vector3(7, 1, 0), new Vector3(7, 1, 7), new Vector3(7, 1, -7), new Vector3(0, 1, 0), new Vector3(0, 1, 7), new Vector3(0, 1, -7), new Vector3(-7, 1, 0), new Vector3(-7, 1, 7) };
+    public GameObject Area;
+    //Vector3[] coord = new[] { new Vector3(7, 1, 0), new Vector3(7, 1, 7), new Vector3(7, 1, -7), new Vector3(0, 1, 0), new Vector3(0, 1, 7), new Vector3(0, 1, -7), new Vector3(-7, 1, 0), new Vector3(-7, 1, 7) };
+    public int Destination;
 
-    public GameObject target;   // change this to edit destination
+
     void Start()
     {
+        Area = GameObject.Find("Area");
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.SetDestination(target.transform.position);
     }
 
 
     void Update()
     {
-        
+        agent.SetDestination(Area.GetComponent<CreateRescueNeeded>().Rooms[Destination].transform.position);
     }
-    public void GetMoveCommand(int n)
-    {
 
+
+    public void GetMoveCommand()
+    {
+        agent.SetDestination(Area.GetComponent<CreateRescueNeeded>().Rooms[Destination].transform.position);
     }
 }
