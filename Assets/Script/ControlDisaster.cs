1f4e1a905a845a76c47964c58b52b4ce44a038b6
Malloc314159 <65201256+Malloc314159@users.noreply.github.com>
2023-05-13 14:48:47 +0900

    Add Script

---
+++ Assets/Script/ControlDisaster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDisaster : MonoBehaviour
+{
+    public GameObject[] Doors = new GameObject[12];
+    public int disasterType; // 0이면 지진, 1이면 화재, 2면 둘다
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
